a602d17169ea8be07dcd01c0344f226ea0b92205
Nexterot <nexterot@mail.ru>
2016-10-28 21:43:02 +0400

    Add files via upload

---
+++ colons.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+def remove_colons(code):
+	""" 
+	заменяет двоеточия на фигурные скобки 
+	code - строка, содержащая sugar-код
+	"""
+	find_colon = code.find(':') # нашли индекс двоеточия
+	if find_colon == -1: # выход из рекурсии
+		return code
+	else:
+		code = code[:find_colon] + '{' + code[find_colon+1:] # 
+		current_index = code[:find_colon].find('\n') # нашли индекс предыдущего \n; текущий индекс
+		count = 0 # счетчик количества пробельных символов в отступе блока
+		while code[current_index].isspace(): # до тех пор пока пробельные символы, проверять следующий символ
+			count += 1
+			current_index += 1
+		count_inner = 0 # счетчик количества пробельных символов в последующих строках, ищем конец вложенного блока
+		while count_inner != count: # идти дальше пока не закончится вложенный блок
+			count_inner = 0
+			current_index = code[current_index:].find('\n')
+			while code[current_index].isspace():
+				count_inner += 1
+				current_index += 1
+		current_index -= count # сдвиг назад
+		code_file = code[:current_index] + '}' + '\n' + code[current_index:] # засунуть в строку '}' и '\n'
+		remove_colons(code) # рекурсивный вызов
+
+if __name__ == '__main__':
+	print("It's not an executive module!")
--- dcp.py
+++ dcp.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-#import string
+
+# функция, меняющая ':' на '{ }'
+from colons import remove_colons
 
 def main():
 	### Чтение файла
@@ -27,39 +29,12 @@ def main():
 		sugar_code = source.read()
 		### Разбор выражений
 		# итоговый файл
-		c_code = remove_colon(sugar_code)
+		c_code = sugar_code
+		c_code = remove_colons(c_code)
 		### Запись си-файла
 		out_file = open(out_file_name, 'w')
 		out_file.write(c_code)
 		out_file.close()
-		
-def remove_colon(code):
-	""" 
-	заменяет двоеточия на фигурные скобки 
-	code - строка, содержащая sugar-код
-	"""
-	find_colon = code.find(':') # нашли индекс двоеточия
-	
-	if find_colon == -1: # выход из рекурсии
-		return code
-	else:
-		code = code[:find_colon-1]+'{'+code[find_colon:]
-		current_index = code[:find_colon].find('\n') # нашли индекс предыдущего \n; текущий индекс
-		count = 0 # счетчик количества пробельных символов в отступе блока
-		while code[current_index].isspace(): # до тех пор пока пробельные символы, проверять следующий символ
-			count += 1
-			current_index += 1
-		count_inner = 0 # счетчик количества пробельных символов в последующих строках, ищем конец вложенного блока
-		while count_inner != count: # идти дальше пока не закончится вложенный блок
-			print(count_inner, current_index)
-			count_inner = 0
-			current_index = code[current_index:].find('\n')
-			while code[current_index].isspace():
-				count_inner += 1
-				current_index += 1
-		current_index -= 1+count # сдвиг назад
-		code_file = code[:current_index] + '}' + '\n' + code[current_index:] # засунуть в строку '}' и '\n'
-		remove_colon(code) # рекурсивный вызов
 	
 if __name__ == '__main__':
 	main()
