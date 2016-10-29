14ed83fee4dab50193d3cc1e3e7420cdfea170b6
Nexterot <nexterot@mail.ru>
2016-10-30 00:20:39 +0300

    modified:   colons.py 	modified:   colons.pyc 	modified:   dcp.py 	modified:   source.c

--- colons.py
+++ colons.py
@@ -14,8 +14,6 @@ def remove_colons(code):
     while index < (len(code)-1) and count_inner >= count:
         count_inner = 0
         index+=code[index:].find('\n')
-<<<<<<< HEAD
-<<<<<<< HEAD
         all_spaces = True
         for c in range(index+1, code.find('\n', index+1)):
             if not code[c].isspace() and all_spaces:
@@ -27,21 +25,7 @@ def remove_colons(code):
             index+=1
     code = code[:index]+"\n"+"}"+"\n"+code[index:]
     return remove_colons(code)
-
-=======
-=======
->>>>>>> refs/remotes/origin/master
-        while index < (len(code)-1) and code[index+1].isspace():
-            count_inner+=1
-            index+=1
-    code = code[:index]+"}"+"\n"+code[index:]
-    return remove_colons(code)
     
-        
-<<<<<<< HEAD
->>>>>>> bc7c690f7c0b5b2611bfead3fbe87bff053c03a9
-=======
->>>>>>> refs/remotes/origin/master
+
 if __name__ == '__main__':
     print("It's not an executive module!")
-i = 5
--- source.c
+++ source.c
@@ -4,19 +4,10 @@ int main(){
 	int num = 10
 	for int i = 0; i < num; i++ {
 		printf("Hello World!\n")
-<<<<<<< HEAD
-<<<<<<< HEAD
+		
 			        
 	return 0
 }
 
-=======
-	
-	return 0}
->>>>>>> bc7c690f7c0b5b2611bfead3fbe87bff053c03a9
-=======
-	
-	return 0}
->>>>>>> refs/remotes/origin/master
 }
 
